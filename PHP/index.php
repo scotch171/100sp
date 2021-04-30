7cd5f9164ab53245cb45d7b223eae9936f11e8a8
Dmitry <56108320+scotch171@users.noreply.github.com>
2021-04-30 12:25:52 +1000

    Fix index.php

--- PHP/index.php
+++ PHP/index.php
@@ -8,16 +8,37 @@ function isPal($str)
     $rev = iconv('windows-1251', 'utf-8', $rev);
 
     if ($str == $rev){
+        if (strlen($str) == 1){
+            return false;
+        }
         echo $str;
         return true;
     } else {
         $str = mb_substr($str, 0, -1);
-        isPal($str);
+        $check = isPal($str);
+        if ($check === true)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+function revIsPal($str)
+{
+    $check = isPal($str);
+    if ($check == false){
+        $strNew = iconv('utf-8', 'windows-1251', $str);
+        $rev = strrev($strNew);
+        $check = isPal($rev);
+        if ($check == false){
+            echo $str[0];
+        }
     }
 }
 
 
 //Задаем строку
-$str = 'Sum summus musafgasg';
+$str = 'sdgjsjdbjghslkjdfgasfaaf';
 //Узнаем
-isPal($str);
+revIsPal($str);
